a7cacacf6bf4125a5af1ec0082aa42b3596dd722
Jani Eväkallio <jani.evakallio@gmail.com>
2016-08-21 12:49:51 +0100

    Remove sample env file (not used)

--- env.example.js
+++
@@ -1,8 +0,0 @@
-// Secrets for the applications. Do NOT commit any secrets to version control.
-// 1. cp env.example.js env.js
-// 2. Fill in the blanks
-
-module.exports = {
-  AUTH0_CLIENT_ID: '',
-  AUTH0_DOMAIN: ''
-};
